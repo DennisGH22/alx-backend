f0779f612753c7e9fa0bc58ef9bb0e427d4d9128
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-10 21:47:07 +0200

    Node Redis client and async operations

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -16,12 +16,9 @@ client.on('error', (err) => {
 
 async function setNewSchool (schoolName, value) {
   try {
-    await setAsync(schoolName, value, (err, reply) => {
-      if (err) {
-        console.error('Error setting value:', err);
-      } else {
-        console.log('Reply:', reply);
-      }
+    await setAsync(schoolName, value);
+    await setAsync((err, reply) => {
+      if (!err) console.log('Reply:', reply);
     });
   } catch (err) {
     console.error(err);
