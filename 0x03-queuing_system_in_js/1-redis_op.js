671620302d11f428588ac15aa94303b01ea8fdcf
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-10 21:09:23 +0200

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -15,7 +15,7 @@ function setNewSchool (schoolName, value) {
     if (err) {
       console.error(err);
     } else {
-      console.log(reply);
+      console.log('Reply: ', reply);
     }
   });
 }
